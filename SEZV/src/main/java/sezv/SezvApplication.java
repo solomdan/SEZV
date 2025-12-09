4a4fc43a19859fe77ca3b6314636263b4b8d467c
Danylo Solomianyi <solomdan@cvut.cz>
2025-12-09 19:09:40 +0100

    init java

---
+++ SEZV/src/main/java/sezv/SezvApplication.java
@@ -0,0 +1,13 @@
+package sezv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SezvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SezvApplication.class, args);
+	}
+
+}
---
+++ SEZV/src/test/java/sezv/SezvApplicationTests.java
@@ -0,0 +1,13 @@
+package sezv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SezvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
